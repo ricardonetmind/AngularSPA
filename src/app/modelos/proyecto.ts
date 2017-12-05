1ac84b881881ac5961b6b190e198202b95790bff
Angular CLI <angular-cli@angular.io>
2017-12-05 13:40:44 +0100

    chore: initial commit from @angular/cli

---
+++ src/app/modelos/proyecto.ts
@@ -0,0 +1,10 @@
+export class Proyecto {
+
+	pid:number;
+	nombre:string;
+
+	constructor (pid:number,nombre:string){
+		this.pid = pid;
+		this.nombre = nombre;
+	}
+}
---
+++ src/app/modelos/tarea.ts
@@ -0,0 +1,14 @@
+export class Tarea {
+
+	tid:number;
+	descripcion:string;
+	tiempo:number;
+	proyecto:number;
+
+	constructor (tid:number,descripcion:string,tiempo:number,proyecto:number){
+		this.tid=tid;
+		this.descripcion=descripcion;
+		this.tiempo=tiempo;
+		this.proyecto=proyecto;
+	}
+}
